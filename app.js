de879156e4743dde199efebfb4bd4dfdc61ecdd8
Mushfiqur Rahman <mushfiq.moon013@gmail.com>
2018-09-06 15:19:34 +0600

    button add in HTML

--- app.js
+++ app.js
@@ -4,7 +4,6 @@
 
 const topB = document.getElementById('top');
 const leftB = document.getElementById('left');
-
 const rightB = document.getElementById('right');
 const bottomB = document.getElementById('bottom');
 
@@ -14,49 +13,64 @@ let ball = document.querySelector('.ball');
 let x = 0;
 let y = 0;
 
+const gameover =  () => console.log('Game Over');
+
 let movingBallRight = function () {
-    if (x>=480){
-        console.log('go back');
-        
+    if (x>=280){
+        gameover()    
     }else{
-        x = x + 10;
+        x = x + 20;
         ball.style.left = `${x}px`;
     }
 }
 
 let movingBallLeft = function () {
     if (x<=0){
-        console.log('go back');
-        
+        gameover()    
     }else{
-        x = x - 10;
+        x = x - 20;
         ball.style.left = `${x}px`;
     }
 }
 
 let movingBallTop = function () {
     if (y<=0){
-        console.log('go back');
-        
+        gameover()    
     }else{
-        y = y - 10;
+        y = y - 20;
         ball.style.top = `${y}px`;
     }
 }
 
 let movingBallBottom = function () {
-    if (y>=480){
-        console.log('go back');
-        
+    if (y>=280){
+        gameover()    
     }else{
-        y = y + 10;
+        y = y + 20;
         ball.style.top = `${y}px`;
     }
 }
 
 
-rightB.addEventListener('click', movingBallRight);
 leftB.addEventListener('click', movingBallLeft);
-
 topB.addEventListener('click', movingBallTop);
+rightB.addEventListener('click', movingBallRight);
 bottomB.addEventListener('click', movingBallBottom);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
