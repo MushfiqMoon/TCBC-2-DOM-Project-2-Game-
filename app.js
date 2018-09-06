5992ba7a3157359ba49e26b4fd3f63d5197189cd
Mushfiqur Rahman <mushfiq.moon013@gmail.com>
2018-09-06 17:43:42 +0600

    without setinterval methode

--- app.js
+++ app.js
@@ -16,52 +16,49 @@ let y = 0;
 const gameover = () => console.log('Game Over');
 
 let movingBallRight = function () {
-    setInterval(function () {
+    
         if (x >= 580) {
             gameover()
         } else {
             x = x + 20;
             ball.style.left = `${x}px`;
         }
-    }, 500)
-
-
-
+    
+    
 }
 
 let movingBallLeft = function () {
-    setInterval(function () {
+    
         if (x <= 0) {
             gameover()
         } else {
             x = x - 20;
             ball.style.left = `${x}px`;
         }
-    }, 500)
+  
 }
 
 let movingBallTop = function () {
-    setInterval(function () {
+   
         if (y <= 0) {
             gameover()
         } else {
             y = y - 20;
             ball.style.top = `${y}px`;
         }
-    }, 500)
+ 
 }
 
 let movingBallBottom = function () {
-    setInterval(function () {
+ 
         if (y >= 380) {
             gameover()
         } else {
             y = y + 20;
             ball.style.top = `${y}px`;
         }
-    }, 500)
-}
 
+}
 
 leftB.addEventListener('click', movingBallLeft);
 topB.addEventListener('click', movingBallTop);
